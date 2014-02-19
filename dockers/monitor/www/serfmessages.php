238b8e1147b413b60ae0e8536ab9a6003632abe2
Jorrit <j.jong@uglyduckling.nl>
2014-02-19 16:27:26 +0100

    first version monitor complete

--- dockers/monitor/www/serfmessages.php
+++ dockers/monitor/www/serfmessages.php
@@ -1,31 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<?php include("header.html");?>
-</head>
-<body>
+	<head>
+		<?php include("header.html");?>
+	</head>
+	<body>
 
-<H1>Serf Agents</H1>
+		<H1>Serf Agents</H1>
 
-<?php 
- include("serf_agent.php");
- listagents($_POST["ip"]); 
-?>
+<div class="serflist">
+		<?php 
+		 include("serf_agent.php");
+		 listagents($_POST["ip"]); 
+		?>
+</div>
+		<H1>Serf listen</H1>
 
-<H1>Serf listen</H1>
-
-<?php 
-	echo "Listening on IP " . $_POST["ip"]; 
-?>
-
-<?php 
- listen($_POST["ip"]); 
-?>
-
-<br>
-<br>
->
-<?php include("footer.html");?>
-
-</body>
+		<?php 
+			echo "Listening on IP " . $_POST["ip"]; 
+		?>
+		
+<div class="serfmsg">
+		<?php 
+		 listen($_POST["ip"]); 
+		?>
+</div>
+		<br>
+		<br>
+		
+		<?php include("footer.html");?>
+	</body>
 </html>
